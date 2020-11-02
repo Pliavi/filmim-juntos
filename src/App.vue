d23233c3e5e74a07bead5f714809630704107830
Pliavi <pliavi.rpgm@gmail.com>
2020-11-02 12:44:43 -0300

    testing peerjs, working!

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <input type="text" v-model="peerID" @keydown.enter="handleKey" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Peer from "peerjs";
+import { ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
+  setup() {
+    const peerID = ref("");
+    const peer = new Peer({ host: "localhost", port: 9000, path: "/" });
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    const handleKey = () => {
+      console.log(peerID.value)
+      const conn = peer.connect(peerID.value);
+      conn.on("open", () => {
+        conn.send("hi!");
+      });
+    };
+
+    peer.on("connection", (conn) => {
+      conn.on("data", (data) => {
+        console.log(data);
+      });
+      conn.on("open", () => {
+        conn.send("hello!");
+      });
+    });
+
+    return { peerID, handleKey };
+  },
+};
+</script>
